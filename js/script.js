a1910e030b19565a229e3da040a2a9947e20f01f
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-11 00:07:23 +0100

    Change index.html, change js, change sass

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var buttonClass = document.getElementsByClassName('button');
+// console.log(buttonClass); 
+
+// console.log(buttonClass.length);
+
+for (var i = 0; i < buttonClass.length; i++) {
+
+	alert(buttonClass[i].innerText);
+
+	console.log(buttonClass[i].innerText)
+}
+
+
+
+
+
+
+
