d6979553c134be63ac2062246e54f479b14cdbe3
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-22 04:03:10 +0100

    Update index.html, update js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 var withButtonClass = document.getElementsByClassName('button');
-// console.log(withButtonClass); 
 
+// console.log(withButtonClass); 
 // console.log(withButtonClass.length);
 
 for (var i = 0; i < withButtonClass.length; i++) {
-
 	alert(withButtonClass[i].innerText);
-
 	console.log(withButtonClass[i].innerText)
 }
 
